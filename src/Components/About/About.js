1f60e04e360a15842b3012d0c61fba210a55be80
xunaid5 <junaid.cou@gmail.com>
2021-12-08 00:31:31 +0600

    done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,13 +10,14 @@ const About = () => {
     return (
        <div data-aos="fade-up">
             <div className="  md:pt-50 md:mt-40 block">
-            <h1 className="text-center head relative text-4xl text-medium  mt-10 ">About</h1>
+            <h1 className="text-center head relative text-4xl text-medium  mt-10 mb-4 ">About</h1>
             <div  className="md:flex justify-evenly md:mt-4">
                 <div className="items-center">
                 <img src={jnd} data-aos="zoom-out" className="mx-auto mb-4  w-1/2" alt=""  />
                 </div>
                 <div data-aos="zoom-out" className="md:w-2/3 text-center md:mt-0 ">
-                <h1 className=" md:text-left font-medium ml-4">I'm Junaid Ahmed</h1>
+                <h1 className=" md:text-left font-large ml-4">I'm Junaid Ahmed</h1>
+                <br />
                  <div className="text-xl">
                  <p className=" mr-4 md:text-justify px-3 ">A <span className="highlight">Front-end Web Developer.</span>
                  <br />
