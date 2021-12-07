c3eca697fe850746857d97bee00b55d1a94f561f
xunaid5 <junaid.cou@gmail.com>
2021-12-08 00:23:00 +0600

    done

--- src/Components/Skills/Skill.js
+++ src/Components/Skills/Skill.js
@@ -6,6 +6,7 @@ const Skill = () => {
                 <h1 className="text-center head relative text-4xl text-medium  mt-10">Skills</h1>
             
             <div className=" md:w-1/2 mx-auto skills shadow-md   ">
+                <div className="skill-container">
                 <h3 className="skill html my-4 relative">HTML <small className="ml-4 text-red-400">90%</small></h3>
                 <h3 className="skill css my-4 relative">CSS  <small className="ml-4 text-red-400">90%</small></h3>
                 <h3 className="skill js my-4 relative ">JavaScript  <small className=" ml-4 text-red-400">70%</small></h3>
@@ -13,7 +14,9 @@ const Skill = () => {
                 <h3 className="skill node my-4 relative">Node JS  <small className=" ml-4 text-red-400">60%</small></h3>
                 <h3 className="skill express my-4 relative">Express JS  <small className="ml-4 text-red-400">65%</small></h3>
                 <h3 className="skill mongo my-4 relative">MongoDb  <small className="ml-4 text-red-400">70%</small></h3>
-            </div>
+            
+                </div>
+                </div>
         </div>
     );
 };
