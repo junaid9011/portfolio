14a9885ee908ab51756f9feeeae1ae63685d7afb
xunaid5 <junaid.cou@gmail.com>
2022-02-11 10:57:07 +0600

    form

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,8 @@ const Footer = () => {
             
              <div className=" mt-4 text-center pt-4 bg-gray-900">
              <h1 className="text-center   relative text-4xl text-medium text-red-500 mt-10">Get In Touch</h1>
-             <input className=" mt-10 md:w-96 rounded leading-8 py-2 border-2  " type="text" placeholder=" Enter Your Name" />
+                <form action="" method="post" data-netlify="true">
+                <input className=" mt-10 md:w-96 rounded leading-8 py-2 border-2  " type="text" placeholder=" Enter Your Name" />
              <br />
              <input className=" mt-10 md:w-96 rounded leading-8 border-2 py-2  " type="text" placeholder=" Enter Your Email" />
              <br />
@@ -16,6 +17,7 @@ const Footer = () => {
              <input className=" mt-2 md:w-96 bg-red-500 text-white p-2 border-2 bg border-red-500 rounded" type="button" value="Submit" />
              <br />
              <br />
+                </form>
              </div>
             
         </div>
