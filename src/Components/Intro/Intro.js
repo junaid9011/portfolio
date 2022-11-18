b6e8c562575adcd0615355d897407e3c27dd7e1b
xunaid5 <junaid.cou@gmail.com>
2022-11-18 23:33:49 +0600

    info

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -34,7 +34,7 @@ const Intro = () => {
                 <div className="links MT-10">
                     <Fade up>
                         <a href="https://www.facebook.com/2a6m9x" rel="noreferrer" target="_blank"><CgFacebook className="m-1 md:m-2 text-xl md:text-3xl" /></a>
-                        <a href="https://github.com/xunaid5" rel="noreferrer" target="_blank"><BsGithub className="m-1 md:m-2 text-xl md:text-3xl" /></a>
+                        <a href="https://github.com/junaid9011" rel="noreferrer" target="_blank"><BsGithub className="m-1 md:m-2 text-xl md:text-3xl" /></a>
                         <a href="https://www.linkedin.com/in/junaid-ahmed-aab938225/" rel="noreferrer" target="_blank"><AiFillLinkedin className="m-1 md:m-2 text-xl md:text-3xl" /></a>
                         {/* <a href=""><CgFacebook className="m-2 text-2xl" /></a> */}
                     </Fade>
