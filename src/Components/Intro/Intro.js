cb44c1211a739b5dff8d3b6242112edbf238dcd1
xunaid5 <junaid.cou@gmail.com>
2022-11-18 23:31:56 +0600

    about

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -9,9 +9,9 @@ const Intro = () => {
     return (
         <div className="body">
             <div className="intro">
-            <Fade top>
+                <Fade top>
                     <h1 className="name" >I'm Junaid</h1>
-                    
+
                     <h1 className="typical">a{''}
                         <Typical
                             steps={[
@@ -33,9 +33,10 @@ const Intro = () => {
                 </Fade>
                 <div className="links MT-10">
                     <Fade up>
-                        <a href="https://www.facebook.com/2a6m9x" rel="noreferrer" target="_blank"><CgFacebook className="m-1 md:m-2 text-xl md:text-3xl" /></a>
-                        <a href="https://github.com/xunaid5" rel="noreferrer" target="_blank"><BsGithub className="m-1 md:m-2 text-xl md:text-3xl" /></a>
+                        <a href="https://github.com/junaid9011" rel="noreferrer" target="_blank"><BsGithub className="m-1 md:m-2 text-xl md:text-3xl" /></a>
                         <a href="https://www.linkedin.com/in/junaid-ahmed-aab938225/" rel="noreferrer" target="_blank"><AiFillLinkedin className="m-1 md:m-2 text-xl md:text-3xl" /></a>
+                        <a href="https://www.facebook.com/2a6m9x" rel="noreferrer" target="_blank"><CgFacebook className="m-1 md:m-2 text-xl md:text-3xl" /></a>
+
                         {/* <a href=""><CgFacebook className="m-2 text-2xl" /></a> */}
                     </Fade>
                 </div>
